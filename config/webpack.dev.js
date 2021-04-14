d481eb85bbc143c554a5fde9e9bf16eb90bb3293
Stas Ilyin <stasilyin.by@gmail.com>
2021-04-14 18:54:29 +0300

    add section our services

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,7 +5,7 @@ const webpackConfigBase = require('./webpack.common');
 module.exports = merge(webpackConfigBase, {
   mode: 'development',
   devServer: {
-    port: 8883,
+    port: 8881,
     hot: true,
     open: true,
   },
